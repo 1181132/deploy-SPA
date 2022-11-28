c48dab24e2d5a994972c1bfebcef545cc8fd1736
Ricardo_Moreira <1191631@isep.ipp.pt>
2022-11-28 23:27:26 +0000

    LAPR5-2 US SPA  criar armazem (pequenas alterações)

---
+++ Armazem_MasterData/Migrations/20221128213343_InitialCreate.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DDDNetCore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Armazens",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Designacao_Designacao = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Endereco_Rua = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Endereco_NumeroPorta = table.Column<int>(type: "int", nullable: true),
+                    Endereco_CodigoPostal = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Endereco_Cidade = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Endereco_Pais = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Coordenadas_CoordenadaLat = table.Column<double>(type: "double", nullable: true),
+                    Coordenadas_CoordenadaLon = table.Column<double>(type: "double", nullable: true),
+                    Altura_Altura = table.Column<double>(type: "double", nullable: true),
+                    Active = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Armazens", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Entregas",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Data_Data = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Massa_Massa = table.Column<int>(type: "int", nullable: true),
+                    ArmazemId = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TempoColocarEntrega_TempoColocarEntrega = table.Column<int>(type: "int", nullable: true),
+                    TempoRetirarEntrega_TempoRetirarEntrega = table.Column<int>(type: "int", nullable: true),
+                    Active = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entregas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Armazens");
+
+            migrationBuilder.DropTable(
+                name: "Entregas");
+        }
+    }
+}
